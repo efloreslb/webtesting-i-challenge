d8dd402434183d61e160c3ab3612786bca406c62
Edgar F <eflores.ef@gmail.com>
2019-06-18 20:52:37 -0700

    initial commit

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,15 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('the enhancer', () => {
+   describe('the repair function', () => {
+      it('should restore the durability', () => {
+         //arrange
+            const longsword = {};
+         //act
+            const result = enhancer.repair(longsword);
+         //assert
+            
+      });
+   });
+});
