bd27bddc993533eb830a4a9c92e2e480f8ac4162
Edgar F <eflores.ef@gmail.com>
2019-06-19 21:02:45 -0700

    tested succeed function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -2,7 +2,7 @@ const enhancer = require('./enhancer.js');
 // test away!
 
 describe('the enhancer', () => {
-   describe('the repair function', () => {
+   describe('repair()', () => {
       it('should restore the durability', () => {
          //arrange
             const item = {
@@ -20,4 +20,33 @@ describe('the enhancer', () => {
             })
       });
    });
+
+   describe('succeed()', () => {
+      it('should increase enhancement by 1 if not 20', () => {
+         const item = {
+            name: '',
+            enhancement: 5,
+            durability: 0
+         }
+         const result = enhancer.succeed(item);
+         expect(result).toEqual({
+            name: '',
+            enhancement: 6,
+            durability: 0
+         })
+
+         const weapon = {
+            name: '',
+            enhancement: 20,
+            durability: 0
+         }
+         const weaponresult = enhancer.succeed(weapon);
+         expect(weaponresult).toEqual({
+            name: '',
+            enhancement: 20,
+            durability: 0
+         })
+
+      })
+   })
 });
