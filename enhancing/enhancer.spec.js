0bdfcd1cb01d2ec42fc091774942ed06d55ef5eb
Edgar F <eflores.ef@gmail.com>
2019-06-19 20:35:00 -0700

    tested repair function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -5,11 +5,19 @@ describe('the enhancer', () => {
    describe('the repair function', () => {
       it('should restore the durability', () => {
          //arrange
-            const longsword = {};
+            const item = {
+               name: '',
+               enhancement: 0,
+               durability: 0
+            };
          //act
-            const result = enhancer.repair(longsword);
+            const result = enhancer.repair(item);
          //assert
-            
+            expect(result).toEqual({
+               name: '',
+               enhancement: 0,
+               durability: 100
+            })
       });
    });
 });
